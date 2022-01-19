de5986a8ad6188fbed22e3c81cf9f2c9a30e9a81
volta2030 <volta2030@gmail.com>
2022-01-20 00:22:57 +0900

    -Refactoring imgkit -Delete data_handler

--- imgkit/index.js
+++ imgkit/index.js
@@ -1,6 +1,58 @@
+const sharp = require("sharp");
+
 class ImageLayer {
   constructor() {
+    //initialize
+    this.imgPanel = document.createElement("div");
+    this.imgPanel.id = "div";
+
     this.canvas = document.createElement("canvas");
+    this.canvas.setAttribute("class", "img-canvas");
+    this.canvas.id = "previewImg";
+
+    this.mainColorBox = document.createElement("div");
+    this.mainColor1 = document.createElement("div");
+    this.mainColor1.id = "mainColor1";
+    this.mainColor1.className = "colorBox";
+
+    this.mainColor2 = document.createElement("div");
+    this.mainColor2.id = "mainColor2";
+    this.mainColor2.className = "colorBox";
+
+    this.mainColor3 = document.createElement("div");
+    this.mainColor3.id = "mainColor3";
+    this.mainColor3.className = "colorBox";
+    //
+    this.imgInfoText = document.createElement("h2");
+    this.imgInfoText.id = "imgInfoText";
+    //
+    this.extensionComboBox = document.createElement("select");
+    this.extensionComboBox.id = "extensionComboBox";
+
+    this.defaultOption = document.createElement("option");
+    this.defaultOption.value = "default";
+    this.pngOption = document.createElement("option");
+    this.pngOption.value = "png";
+    this.pngOption.innerText = "png";
+    this.jpgOption = document.createElement("option");
+    this.jpgOption.value = "jpg";
+    this.jpgOption.innerText = "jpg";
+    this.jpegOption = document.createElement("option");
+    this.jpegOption.value = "jpeg";
+    this.jpegOption.innerText = "jpeg";
+    this.webpOption = document.createElement("option");
+    this.webpOption.value = "webp";
+    this.webpOption.innerText = "webp";
+
+    // <select id="extensionComboBox">
+    //   <option value="default"></option>
+    //   <option value="png">png</option>
+    //   <option value="jpg">jpg</option>
+    //   <option value="jpeg">jpeg</option>
+    //   <option value="webp">webp</option>
+    // </select>;
+
+    this.ctx = this.canvas.getContext("2d");
 
     this.bufferQueue = [];
     this.infoQueue = [];
@@ -14,6 +66,173 @@ class ImageLayer {
 
     this.realPosX;
     this.realPosY;
+
+    this.image = new Image();
+    this.filepath;
+
+    this.attach();
+  }
+
+  attach() {
+    this.imgPanel.appendChild(this.canvas);
+    this.imgPanel.appendChild(this.mainColorBox);
+    this.imgPanel.appendChild(this.imgInfoText);
+    this.imgPanel.appendChild(this.extensionComboBox);
+
+    this.mainColorBox.appendChild(this.mainColor1);
+    this.mainColorBox.appendChild(this.mainColor2);
+    this.mainColorBox.appendChild(this.mainColor3);
+
+    this.extensionComboBox.appendChild(this.defaultOption);
+    this.extensionComboBox.appendChild(this.pngOption);
+    this.extensionComboBox.appendChild(this.jpgOption);
+    this.extensionComboBox.appendChild(this.jpegOption);
+    this.extensionComboBox.appendChild(this.webpOption);
+  }
+
+  updatePreviewImg(buf, info, extension) {
+    canvas.width = info.width;
+    canvas.height = info.height;
+
+    image.src = `data:image/${extension};base64, ` + buf.toString("base64");
+    image.onload = () => {
+      this.ctx.drawImage(image, 0, 0, info.width, info.height);
+    };
+
+    this.updateImgInfoText(info);
+    this.extractMainColors(buf, info);
+    this.updateExtension(extension);
+
+    buffer = buf;
+    i++;
+
+    if (i > 10) {
+      bufferQueue.shift();
+      infoQueue.shift();
+      extensionQueue.shift();
+      i--;
+    } else {
+      if (bufferQueue[i + 1] !== null) {
+        bufferQueue = bufferQueue.slice(0, i);
+        infoQueue = infoQueue.slice(0, i);
+        extensionQueue = extensionQueue.slice(0, i);
+      }
+    }
+    bufferQueue.push(buf);
+    infoQueue.push(info);
+    extensionQueue.push(extension);
+  }
+
+  updateImgInfoText(info) {
+    this.imgInfoText.innerText = `${info.width} x ${info.height}`;
+  }
+
+  extractMainColors(buffer, info) {
+    //initialize
+    this.mainColor1.style.background = null;
+    this.mainColor1.title = "empty";
+    this.mainColor2.style.background = null;
+    this.mainColor2.title = "empty";
+    this.mainColor3.style.background = null;
+    this.mainColor3.title = "empty";
+
+    sharp(buffer)
+      .resize({ width: info.width > 24 ? 24 : info.width })
+      .toColorspace("srgb")
+      .raw()
+      .toBuffer((err, result, info) => {
+        var colors = {};
+        var step;
+        var coef = info.width * info.height * 4 === result.length ? 4 : 3;
+        var size = result.length / coef;
+
+        for (step = 0; step < size; step++) {
+          if (
+            colors[
+              `${result[coef * step]} ${result[coef * step + 1]} ${
+                result[coef * step + 2]
+              }`
+            ] !== undefined
+          ) {
+            colors[
+              `${result[coef * step]} ${result[coef * step + 1]} ${
+                result[coef * step + 2]
+              }`
+            ] += 1;
+          } else {
+            colors[
+              `${result[coef * step]} ${result[coef * step + 1]} ${
+                result[coef * step + 2]
+              }`
+            ] = 1;
+          }
+        }
+        const sortColors = Object.entries(colors)
+          .sort(([, a], [, b]) => b - a)
+          .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+
+        var rgb = Object.keys(sortColors)[0].split(" ");
+        var hexColor = `#${
+          parseInt(rgb[0], 10).toString(16).padStart(2, "0") +
+          parseInt(rgb[1], 10).toString(16).padStart(2, "0") +
+          parseInt(rgb[2], 10).toString(16).padStart(2, "0")
+        }`;
+        this.mainColor1.style.background = hexColor;
+        this.mainColor1.title = hexColor;
+
+        if (Object.keys(sortColors).length >= 2) {
+          rgb = Object.keys(sortColors)[1].split(" ");
+          hexColor = `#${
+            parseInt(rgb[0], 10).toString(16).padStart(2, "0") +
+            parseInt(rgb[1], 10).toString(16).padStart(2, "0") +
+            parseInt(rgb[2], 10).toString(16).padStart(2, "0")
+          }`;
+          this.mainColor2.style.background = hexColor;
+          this.mainColor2.title = hexColor;
+        }
+
+        if (Object.keys(sortColors).length >= 3) {
+          rgb = Object.keys(sortColors)[2].split(" ");
+          hexColor = `#${
+            parseInt(rgb[0], 10).toString(16).padStart(2, "0") +
+            parseInt(rgb[1], 10).toString(16).padStart(2, "0") +
+            parseInt(rgb[2], 10).toString(16).padStart(2, "0")
+          }`;
+          this.mainColor3.style.background = hexColor;
+          this.mainColor3.title = hexColor;
+        }
+      });
+  }
+
+  updateExtension(extension) {
+    this.extensionComboBox.value = extension;
+  }
+
+  openImg(filepath, extension) {
+    sharp(filepath).toBuffer((err, buf, info) => {
+      this.updatePreviewImg(buf, info, extension);
+    });
+  }
+
+  saveImg(filepath, extension) {
+    var base64Data = this.image.src.replace(
+      `data:image/${extension};base64,`,
+      ""
+    );
+
+    require("fs").writeFile(filepath, base64Data, "base64", (err) => {
+      if (err) {
+        // console.log("failed to save");
+      } else {
+        // console.log("saved successfully");
+        document
+          .getElementById("save_msg")
+          .animate([{ opacity: "1" }, { opacity: "0" }], {
+            duration: 1800,
+            iterations: 1,
+          });
+      }
+    });
   }
 }
 
--- renderer/main_renderer.js
+++ renderer/main_renderer.js
@@ -1,9 +1,7 @@
 const { ipcRenderer } = require("electron");
+const { ImageLayer, openImg } = require("imgkit");
 const sharp = require("sharp");
 var path = require("path");
-
-// const ImageLayer = require("imgkit");
-
 var filepath = null;
 var buffer = null;
 var extension = null;
@@ -22,7 +20,7 @@ ipcRenderer.on("resizeImgCMD", (event, res) => {
   sharp(buffer)
     .resize({ width: document.getElementById("previewImg").width * res })
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -30,7 +28,7 @@ ipcRenderer.on("blurImgCMD", (event, res) => {
   sharp(buffer)
     .blur(res)
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -38,7 +36,7 @@ ipcRenderer.on("sharpenImgCMD", (event, res) => {
   sharp(buffer)
     .sharpen(res, 1.0, 2.0)
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -46,7 +44,7 @@ ipcRenderer.on("normalizeImgCMD", (event) => {
   sharp(buffer)
     .normalize(true)
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -54,7 +52,7 @@ ipcRenderer.on("medianImgCMD", (event, res) => {
   sharp(buffer)
     .median(res)
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -62,7 +60,7 @@ ipcRenderer.on("rotateImgCMD", (event, res) => {
   sharp(buffer)
     .rotate(res)
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -70,7 +68,7 @@ ipcRenderer.on("rotateRightImgCMD", (event) => {
   sharp(buffer)
     .rotate(90)
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -78,7 +76,7 @@ ipcRenderer.on("rotateLeftImgCMD", (event) => {
   sharp(buffer)
     .rotate(-90)
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -86,7 +84,7 @@ ipcRenderer.on("flipImgCMD", (event) => {
   sharp(buffer)
     .flip()
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -94,7 +92,7 @@ ipcRenderer.on("flopImgCMD", (event) => {
   sharp(buffer)
     .flop()
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -102,7 +100,7 @@ ipcRenderer.on("bitwiseImgCMD", (event) => {
   sharp(buffer)
     .threshold()
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -110,7 +108,7 @@ ipcRenderer.on("negativeImgCMD", (event) => {
   sharp(buffer)
     .negate(true)
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -118,7 +116,7 @@ ipcRenderer.on("grayScaleImgCMD", (event) => {
   sharp(buffer)
     .grayscale(true)
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
@@ -126,27 +124,227 @@ ipcRenderer.on("tintImgCMD", (event, res) => {
   sharp(buffer)
     .tint(res)
     .toBuffer((err, buf, info) => {
-      updatePreviewImg(buf, info, extension);
+      imageLayer.updatePreviewImg(buf, info, extension);
     });
 });
 
-document
-  .getElementById("extensionComboBox")
-  .addEventListener("change", (event) => {
-    extension = event.target.value;
-    sharp(buffer)
-      .toFormat(extension)
-      .toBuffer((err, buf, info) => {
-        buffer = buf;
-        updatePreviewImg(buf, info, extension);
-        document
-          .getElementById("convert_msg")
-          .animate([{ opacity: "1" }, { opacity: "0" }], {
-            duration: 1800,
-            iterations: 1,
-          });
-      });
-  });
+var bufferQueue = [];
+var infoQueue = [];
+var extensionQueue = [];
+var i = -1;
+var cropOption = false;
+var isDrag;
+
+const imageLayer = new ImageLayer();
+const canvas = imageLayer.canvas;
+
+const imgPanel = document.body.appendChild(imageLayer.imgPanel);
+// imgPanel.appendChild(imageLayer.mainColorBox);
+// imageLayer.imgPanel.appendChild(imageLayer.extensionComboBox);
+//document.getElementById("imgPanel").appendChild(canvas);
+
+// const canvas = document.getElementById("previewImg");
+var sioCheckBox = document.getElementById("showImageOnlyCheckBox");
+const cropBtn = document.getElementById("cropBtn");
+// var ctx = canvas.getContext("2d");
+
+sioCheckBox.addEventListener("click", (event) => {
+  if (sioCheckBox.checked) {
+    document.getElementById("mainColorBox").style.visibility = "hidden";
+    document.getElementById("imgInfoText").style.visibility = "hidden";
+    imageLayer.extensionComboBox.style.visibility = "hidden";
+  } else {
+    document.getElementById("mainColorBox").style.visibility = "visible";
+    document.getElementById("imgInfoText").style.visibility = "visible";
+    imageLayer.extensionComboBox.style.visibility = "visible";
+  }
+});
+
+canvas.addEventListener("mousedown", (event) => {
+  isDrag = true;
+  if (cropOption) {
+    var ctxs = canvas.getContext("2d");
+    imageLayer.realPosX = event.clientX;
+    imageLayer.realPosY = event.clientY;
+    imageLayer.initialX = event.clientX - canvas.getBoundingClientRect().left;
+    imageLayer.initialY = event.clientY - canvas.getBoundingClientRect().top;
+    ctxs.setLineDash([2]);
+
+    canvas.addEventListener("mousemove", (evt) => {
+      if (isDrag && cropOption) {
+        ctxs.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+        imageLayer.cropWidth = evt.clientX - event.clientX;
+        imageLayer.cropHeight = evt.clientY - event.clientY;
+        ctxs.drawImage(image, 0, 0);
+        ctxs.strokeRect(
+          imageLayer.initialX,
+          imageLayer.initialY,
+          imageLayer.cropWidth,
+          imageLayer.cropHeight
+        );
+      }
+    });
+  }
+});
+
+document.body.addEventListener("mouseup", (event) => {
+  isDrag = false;
+  if (
+    cropOption &&
+    event.x - imageLayer.realPosX < canvas.clientWidth &&
+    event.y - imageLayer.realPosY < canvas.clientHeight
+  ) {
+    if (
+      imageLayer.cropWidth < 0 ||
+      imageLayer.cropHeight < 0 ||
+      imageLayer.cropWidth > canvas.clientWidth ||
+      imageLayer.cropHeight > canvas.clientHeight
+    ) {
+      var ctxs = canvas.getContext("2d");
+      ctxs.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+      ctxs.drawImage(image, 0, 0);
+    } else {
+      sharp(buffer)
+        .extract({
+          left: parseInt(imageLayer.initialX),
+          top: parseInt(imageLayer.initialY),
+          width: parseInt(imageLayer.cropWidth),
+          height: parseInt(imageLayer.cropHeight),
+        })
+        .toBuffer((err, buf, info) => {
+          imageLayer.updatePreviewImg(buf, info, extension);
+        });
+    }
+  }
+});
+
+const image = imageLayer.image;
+imageLayer.openImg("./assets/addImage.png", "png");
+
+ipcRenderer.on("openImgCMD", (event, res) => {
+  filepath = res;
+  if (filepath === undefined || filepath === null) {
+    imageLayer.openImg("./assets/addImage.png", "png");
+  } else {
+    extension = path.extname(filepath).replace(".", "");
+    imageLayer.openImg(filepath, extension);
+  }
+});
+
+ipcRenderer.on("setExtensionCMD", (event) => {
+  ipcRenderer.send("extensionValueSEND", extension);
+});
+
+ipcRenderer.on("saveImgCMD", (event) => {
+  imageLayer.saveImg(filepath, extension);
+});
+
+ipcRenderer.on("saveAsImgCMD", (event, res) => {
+  imageLayer.saveImg(res, extension);
+  filepath = res;
+});
+
+function undoPreviewImg() {
+  if (i === 0) return;
+
+  i--;
+  console.log(i);
+  try {
+    buffer = bufferQueue[i];
+    info = infoQueue[i];
+    extension = extensionQueue[i];
+
+    canvas.width = infoQueue[i].width;
+    canvas.height = infoQueue[i].height;
+
+    image.src = `data:image/${extension};base64, ` + buffer.toString("base64");
+    image.onload = () => {
+      imageLayer.ctx.drawImage(image, 0, 0);
+    };
+
+    imageLayer.updateImgInfoText(info);
+    imageLayer.extractMainColors(buffer, info);
+    imageLayer.updateExtension(extension);
+  } catch (err) {
+    i++;
+  }
+}
+
+function redoPreviewImg() {
+  i++;
+  try {
+    buffer = bufferQueue[i];
+    info = infoQueue[i];
+    extension = extensionQueue[i];
+
+    canvas.width = infoQueue[i].width;
+    canvas.height = infoQueue[i].height;
+
+    image.src = `data:image/${extension};base64, ` + buffer.toString("base64");
+    image.onload = () => {
+      imageLayer.ctx.drawImage(image, 0, 0);
+    };
+
+    imageLayer.updateImgInfoText(info);
+    imageLayer.extractMainColors(buffer, info);
+    imageLayer.updateExtension(extension);
+  } catch (err) {
+    i--;
+  }
+}
+
+canvas.addEventListener("drag", function (event) {}, false);
+
+canvas.addEventListener(
+  "dragover",
+  function (event) {
+    event.preventDefault();
+  },
+  false
+);
+
+canvas.addEventListener(
+  "drop",
+  function (event) {
+    event.preventDefault();
+    filepathTmp = event.dataTransfer.files[0]["path"];
+    extension = path.extname(filepath).replace(".", "");
+    imageLayer.openImg(filepathTmp, extension);
+  },
+  false
+);
+
+cropBtn.addEventListener("click", (event) => {
+  isDrag = false;
+  // initialX = initialY = cropWidth = cropHeight = 0;
+  if (!cropOption) {
+    canvas.setAttribute("draggable", false);
+    document.body.style.cursor = "crosshair";
+    cropBtn.style.backgroundColor = "gray";
+    cropOption = true;
+  } else {
+    canvas.setAttribute("draggable", true);
+    document.body.style.cursor = "default";
+    cropBtn.style.backgroundColor = "";
+    cropOption = false;
+  }
+});
+
+imageLayer.extensionComboBox.addEventListener("change", (event) => {
+  extension = event.target.value;
+  sharp(buffer)
+    .toFormat(extension)
+    .toBuffer((err, buf, info) => {
+      buffer = buf;
+      imageLayer.updatePreviewImg(buf, info, extension);
+      document
+        .getElementById("convert_msg")
+        .animate([{ opacity: "1" }, { opacity: "0" }], {
+          duration: 1800,
+          iterations: 1,
+        });
+    });
+});
 
 document.getElementById("mainColor1").addEventListener("click", (event) => {
   const text = document.createElement("textarea");
